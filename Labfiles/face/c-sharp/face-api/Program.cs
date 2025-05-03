f1b0f8de97de07ea864ba17ae92b5d61397709b4
olprod <olprod@microsoft.com>
2025-05-02 20:05:38 -0700

    Copy Files From Source Repo (2025-05-02 20:05)

---
+++ Labfiles/face/c-sharp/face-api/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Azure;
+using SkiaSharp;
+
+// Import namespaces
+
+
+
+namespace analyze_faces
+{
+    class Program
+    {
+
+        // Declare variable for Face client
+
+
+
+        static async Task Main(string[] args)
+        {
+            // Clear the console
+            Console.Clear();
+            
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string cogSvcEndpoint = configuration["AIServicesEndpoint"];
+                string cogSvcKey = configuration["AIServiceKey"];
+
+                // Get image
+                string imageFile = "images/face1.jpg";
+                if (args.Length > 0)
+                {
+                    imageFile = args[0];
+                }
+
+                // Authenticate Face client
+
+
+
+                // Detect faces in the image
+                await DetectFaces(imageFile);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task DetectFaces(string imageFile)
+        {
+            Console.WriteLine($"Detecting faces in {imageFile}");
+
+            // Specify facial features to be retrieved
+
+
+            // Get faces
+ 
+ 
+        }
+    }
+}
